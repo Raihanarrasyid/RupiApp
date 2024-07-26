6fcb15be3a36d4ccad609b087a0970c00b4d82c3
desendoo <desendoimanuel46@gmail.com>
2024-07-26 11:12:19 +0700

    Added AccountService API Docs

--- src/main/java/com/team7/rupiapp/dto/account/AccountDetailResponseDto.java
+++ src/main/java/com/team7/rupiapp/dto/account/AccountDetailResponseDto.java
@@ -1,11 +1,21 @@
 package com.team7.rupiapp.dto.account;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Account detail response")
 public class AccountDetailResponseDto {
+
+    @Schema(description = "Full name of account holder", example = "John Doe")
     private String fullName;
+
+    @Schema(description = "Email of account holder", example = "john.doe@example.com")
     private String email;
+
+    @Schema(description = "Account number", example = "1234567890")
     private String accountNumber;
+
+    @Schema(description = "Account balance", example = "1500000.0")
     private Double balance;
 }
